3b5b5e96a327124c5a528d6c8ae3a39d5bb34509
Marc Jansing <marc.jansing@innoq.com>
2018-05-15 06:41:04 +0200

    add proof of concept setup

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env zsh
+
+ln -s ./zshrc ~/.zshrc
+
+# vim
+ln -s ~/dotfiles/vim ~/.vim
+ln -s ~/dotfiles/vimrc ~/.vimrc
+
+# git
+ln -s ~/dotfiles/gitconfig ~/.gitconfig
+
+# tmux
+# TODO: add reattach-to-users-namespace
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+
